14b01f2a80784d00444eb857d79f41f9dece177d
James Roscoe <jamesroscoe@Jamess-iMac.local>
2016-08-23 11:41:31 -0400

    added some debug messaging

--- lib/degzipper/middleware.rb
+++ lib/degzipper/middleware.rb
@@ -6,12 +6,18 @@ module Degzipper
 
     def call(env)
       if method_handled?(env['REQUEST_METHOD']) && encoding_handled?(env['HTTP_CONTENT_ENCODING'])
-        extracted = decode(env['rack.input'], env['HTTP_CONTENT_ENCODING'])
+        puts "env['CONTENT_LENGTH']: #{env['CONTENT_LENGTH']}"
+        puts env['rack.input'].read
+        env['rack.input'].rewind
+        extracted = decode(env['rack.input'], env['HTTP_CONTENT_ENCODING']).tap {|e| puts e}
 
         env.delete('HTTP_CONTENT_ENCODING')
         env['CONTENT_LENGTH'] = extracted.bytesize
         env['rack.input'] = StringIO.new(extracted).set_encoding('utf-8')
       end
+      puts "env['rack.input'].size: #{env['rack.input'].size}"
+      puts env['rack.input'].read
+      env['rack.input'].rewind
 
       @app.call(env)
     end
@@ -27,6 +33,9 @@ module Degzipper
     end
 
     def decode(input, content_encoding)
+      puts "input.size: #{input.size}"
+      puts "input.bytes: #{input.bytes}"
+      input.rewind
       # type of input depends on CONTENT_LENGTH
       # if CONTENT_LENGTH < 20k it's StringIO; if more it's Tempfile
       # that's why use only common methods of these types
--- lib/degzipper/rails.rb
+++ lib/degzipper/rails.rb
@@ -1,7 +1,7 @@
 module Degzipper
   class Rails < Rails::Railtie
     initializer "degzipper.configure_rails_initialization" do |app|
-      app.middleware.insert_before ActionDispatch::ParamsParser, "Degzipper::Middleware"
+      app.middleware.insert_before ActionDispatch::Static, "Degzipper::Middleware"
     end
   end
 end
